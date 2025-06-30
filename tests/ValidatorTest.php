4beba4b860563c9a860ba9a9d6788f9f2264c991
Bonifac <kolenomi@seznam.cz>
2025-06-30 15:19:30 +0200

    updated tests

--- tests/ValidatorTest.php
+++ tests/ValidatorTest.php
@@ -3,24 +3,29 @@
 use PHPUnit\Framework\TestCase;
 use bonifac0\VytrvalecValidator\Validator;
 
-
 class ValidatorTest extends TestCase
 {
     private Validator $validator;
-    private string $imagePath;
+    private string $imagePathGood;
+    private string $imagePathFish;
+
 
     protected function setUp(): void
     {
+        echo "this will take 2-3 minutes";
         $this->validator = new Validator();
 
-        $this->imagePath = __DIR__ . '/../testdata/test_image.jpg';
-        $this->assertFileExists($this->imagePath, "Test image file missing.");
+        $this->imagePathGood = __DIR__ . '/../testdata/test_image_good.jpg';
+        $this->assertFileExists($this->imagePathGood, "Test image file missing.");
+
+        $this->imagePathFish = __DIR__ . '/../testdata/fish.jpg';
+        $this->assertFileExists($this->imagePathFish, "Fish image file missing.");
     }
 
-    public function testFullValidationReturnsExpectedFormat()
+    public function testFullValidationAccept()
     {
         [$isValid, $errorCode] = $this->validator->validate(
-            $this->imagePath,
+            $this->imagePathGood,
             distance: 7900,
             elevation: 110,
             makelogs: false
@@ -28,33 +33,61 @@ class ValidatorTest extends TestCase
 
         $this->assertIsBool($isValid);
         $this->assertIsInt($errorCode);
+        $this->assertTrue($isValid);
+        $this->assertEquals(0, $errorCode);
+    }
+
+    public function testValidationRejectCodes()
+    {
+        [$isValid, $errorCode] = $this->validator->validate(
+            $this->imagePathFish,
+            distance: 7900,
+            elevation: 110,
+            makelogs: false
+        );
+        $this->assertFalse($isValid);
+        $this->assertEquals(1, $errorCode);
+
+        $inference = $this->invokePrivateMethod($this->validator, 'runInference', [$this->imagePathGood]);
+
+        [$isValid, $errorCode] = $this->invokePrivateMethod($this->validator, 'accept', [$inference, 4200, 110]);
+        $this->assertFalse($isValid);
+        $this->assertEquals(2, $errorCode);
+
+        [$isValid, $errorCode] = $this->invokePrivateMethod($this->validator, 'accept', [$inference, 7900, 42]);
+        $this->assertFalse($isValid);
+        $this->assertEquals(3, $errorCode);
     }
 
     public function testRunInferenceReturnsValidStructure()
     {
-        $data = $this->invokePrivateMethod($this->validator, 'runInference', [$this->imagePath]);
+        $data = $this->invokePrivateMethod($this->validator, 'runInference', [$this->imagePathGood]);
 
         $this->assertIsArray($data);
-        $this->assertArrayHasKey('distance', $data);
         $this->assertArrayHasKey('valid_rules', $data);
+
+        $datafish = $this->invokePrivateMethod($this->validator, 'runInference', [$this->imagePathFish]);
+
+        $this->assertIsArray($datafish);
+        $this->assertArrayHasKey('valid_rules', $datafish);
     }
+
 
     public function testExtractDataReturnsFields()
     {
-        $data = $this->invokePrivateMethod($this->validator, 'extractData', [$this->imagePath]);
+        $data = $this->invokePrivateMethod($this->validator, 'extractData', [$this->imagePathGood]);
 
         $this->assertIsArray($data);
         $this->assertArrayHasKey('distance', $data);
-        $this->assertArrayHasKey('type_of_excercise', $data);
+        $this->assertArrayHasKey('elevation', $data);
     }
 
-    public function testAssessRuleValidityIncludesFlag()
+    public function testRejectFraud()
     {
-        $data = $this->invokePrivateMethod($this->validator, 'assessRuleValidity', [$this->imagePath]);
-
-        $this->assertIsArray($data);
-        $this->assertArrayHasKey('valid_rules', $data);
+        #TODO add fraud filter and testing
+        $this->assertTrue(true);
     }
+
 
     private function invokePrivateMethod(object $object, string $methodName, array $args = []): mixed
     {
